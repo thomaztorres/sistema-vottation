b3f2713498189b74910da1794b8be8af4221b05e
Thomaz Torres <torresthomaz648@gmail.com>
2022-07-12 02:09:35 -0300

    first day of job, everything working

---
+++ resources/views/home.blade.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/style-config.css') }}">
+        <link rel="stylesheet" href="{{ URL::asset('css/home.css') }}">
+        <title>Vottation</title>
+    </head>
+    <body>
+        <header>
+            <div class="header-items">
+                <div class="logo">
+                    <h1><a href="/home">Vottation</a></h1>
+                </div>
+                <div class="links">
+                    <ul class="links-list">
+                        <li id="active"><a href="/home">Enquetes</a></li>
+                        <li><a href="/newSurvey">Nova Enquete</a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+        <main>
+            <div class="grid-container">
+                <div class="title-section">Abertas</div>
+                <div class="card-section">
+                    <div class="card">
+                        <h3>Lula x Bolsonaro</h3>
+                        <p class="date">11/07/22 - 31/07/22</p>
+                        <div class="card-votes-edit">
+                            <p><b>12</b> Votos</p>
+                            <img src="{{ URL::asset('img/icons/edit.svg') }}" alt="Editar">
+                        </div>
+                    </div>
+                    <div class="card">
+                        <h3>Lula x Bolsonaro</h3>
+                        <p class="date">11/07/22 - 31/07/22</p>
+                        <div class="card-votes-edit">
+                            <p><b>12</b> Votos</p>
+                            <img src="{{ URL::asset('img/icons/edit.svg') }}" alt="Editar">
+                        </div>
+                    </div>
+                    <div class="card">
+                        <h3>Lula x Bolsonaro</h3>
+                        <p class="date">11/07/22 - 31/07/22</p>
+                        <div class="card-votes-edit">
+                            <p><b>12</b> Votos</p>
+                            <img src="{{ URL::asset('img/icons/edit.svg') }}" alt="Editar">
+                        </div>
+                    </div>
+                    <div class="card">
+                        <h3>Lula x Bolsonaro</h3>
+                        <p class="date">11/07/22 - 31/07/22</p>
+                        <div class="card-votes-edit">
+                            <p><b>12</b> Votos</p>
+                            <img src="{{ URL::asset('img/icons/edit.svg') }}" alt="Editar">
+                        </div>
+                    </div>
+                    <div class="card">
+                        <h3>Lula x Bolsonaro</h3>
+                        <p class="date">11/07/22 - 31/07/22</p>
+                        <div class="card-votes-edit">
+                            <p><b>12</b> Votos</p>
+                            <img src="{{ URL::asset('img/icons/edit.svg') }}" alt="Editar">
+                        </div>
+                    </div>
+                </div>
+                <div class="title-section1">Fechadas</div>
+                <div class="card-section">
+                    <div class="card">
+                        <h3>Lula x Bolsonaro</h3>
+                        <p class="date">11/07/22 - 31/07/22</p>
+                        <div class="card-votes-edit">
+                            <p><b>12</b> Votos</p>
+                            <img src="{{ URL::asset('img/icons/edit.svg') }}" alt="Editar">
+                        </div>
+                    </div>
+                    <div class="card">
+                        <h3>Lula x Bolsonaro</h3>
+                        <p class="date">11/07/22 - 31/07/22</p>
+                        <div class="card-votes-edit">
+                            <p><b>12</b> Votos</p>
+                            <img src="{{ URL::asset('img/icons/edit.svg') }}" alt="Editar">
+                        </div>
+                    </div>
+                    <div class="card">
+                        <h3>Lula x Bolsonaro</h3>
+                        <p class="date">11/07/22 - 31/07/22</p>
+                        <div class="card-votes-edit">
+                            <p><b>12</b> Votos</p>
+                            <img src="{{ URL::asset('img/icons/edit.svg') }}" alt="Editar">
+                        </div>
+                    </div>
+                    <div class="card">
+                        <h3>Lula x Bolsonaro</h3>
+                        <p class="date">11/07/22 - 31/07/22</p>
+                        <div class="card-votes-edit">
+                            <p><b>12</b> Votos</p>
+                            <img src="{{ URL::asset('img/icons/edit.svg') }}" alt="Editar">
+                        </div>
+                    </div>
+                    <div class="card">
+                        <h3>Lula x Bolsonaro</h3>
+                        <p class="date">11/07/22 - 31/07/22</p>
+                        <div class="card-votes-edit">
+                            <p><b>12</b> Votos</p>
+                            <img src="{{ URL::asset('img/icons/edit.svg') }}" alt="Editar">
+                        </div>
+                    </div>
+                </div>
+                <div class="title-section2">Futuras</div>
+                <div class="card-section">
+                    <div class="card">
+                        <h3>Lula x Bolsonaro</h3>
+                        <p class="date">11/07/22 - 31/07/22</p>
+                        <div class="card-votes-edit">
+                            <p><b>12</b> Votos</p>
+                            <img src="{{ URL::asset('img/icons/edit.svg') }}" alt="Editar">
+                        </div>
+                    </div>
+                    <div class="card">
+                        <h3>Lula x Bolsonaro</h3>
+                        <p class="date">11/07/22 - 31/07/22</p>
+                        <div class="card-votes-edit">
+                            <p><b>12</b> Votos</p>
+                            <img src="{{ URL::asset('img/icons/edit.svg') }}" alt="Editar">
+                        </div>
+                    </div>
+                    <div class="card">
+                        <h3>Lula x Bolsonaro</h3>
+                        <p class="date">11/07/22 - 31/07/22</p>
+                        <div class="card-votes-edit">
+                            <p><b>12</b> Votos</p>
+                            <img src="{{ URL::asset('img/icons/edit.svg') }}" alt="Editar">
+                        </div>
+                    </div>
+                    <div class="card">
+                        <h3>Lula x Bolsonaro</h3>
+                        <p class="date">11/07/22 - 31/07/22</p>
+                        <div class="card-votes-edit">
+                            <p><b>12</b> Votos</p>
+                            <img src="{{ URL::asset('img/icons/edit.svg') }}" alt="Editar">
+                        </div>
+                    </div>
+                    <div class="card">
+                        <h3>Lula x Bolsonaro</h3>
+                        <p class="date">11/07/22 - 31/07/22</p>
+                        <div class="card-votes-edit">
+                            <p><b>12</b> Votos</p>
+                            <img src="{{ URL::asset('img/icons/edit.svg') }}" alt="Editar">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="menu-smartphone">
+                
+            </div>
+        </main>
+        <footer>
+            &copy; Dev. Thomaz Torres
+        </footer>
+    </body>
+</html>
